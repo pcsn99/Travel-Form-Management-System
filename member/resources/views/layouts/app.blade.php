2d3636fae3e4e05f9b5595118da8646cfe127d3b
Pruweezen <20220025147@my.xu.edu.ph>
2025-05-03 09:06:08 +0000

    github aut

--- member/resources/views/layouts/app.blade.php
+++ member/resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <!-- ✅ CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+<!-- github auth -->
     <!-- ✅ Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
