626968f4e922aaaf3506f02662421df866d3c926
Paul Navidad <161107410+pcsn99@users.noreply.github.com>
2025-05-07 20:53:32 +0800

    middleware fix

--- member/app/Http/Middleware/MemberAuthMiddleware.php
+++ member/app/Http/Middleware/MemberAuthMiddleware.php
@@ -13,6 +13,10 @@ class MemberAuthMiddleware
             return redirect()->route('login');
         }
 
-        return $next($request);
+        $response = $next($request);
+
+        return $response->header('Cache-Control', 'no-cache, no-store, must-revalidate')
+                        ->header('Pragma', 'no-cache')
+                        ->header('Expires', '0');
     }
 }
